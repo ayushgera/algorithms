59f407bc9531c825cd8cdfe1a4b575858f4d7909
Ayush Gera <ayushgera@gmail.com>
2019-02-25 16:44:05 +0800

    subset sum using tabulation

--- DynamicProgramming/SubsetSum.js
+++ DynamicProgramming/SubsetSum.js
@@ -42,4 +42,61 @@
     console.log(subsetSums)
  }
 
- subsetSumBruteForce([3,5,8,1,2,7],9);
+/**
+ * sort the inital array
+ * 
+ * A map of [n] keys (length of main array) x [sum] values. Any entry A[i][j] is 1, if
+ * there exists a subset {a[0],a[1],...a[i]} with sum = j; 0 otherwise
+ * 
+ * Complexity: O(nlogn + n*sum)
+ */
+function subsetSumDP(a, sum){
+    // subset sum map
+    // first key's values can be filled by comparing the number with the sum
+    // since set has only 1 number, which shoes the sum
+    // also sum=0 is always possible by any set, so first column has all 1
+    var subsum={};
+    a=a.sort(); // important, else it becomes impossible to get the lists
+    // sorting not needed if only expect ouput is the length
+    for(var i=0;i<a.length;i++){
+        if(!subsum[a[i]]){
+            subsum[a[i]]=[];
+        }
+        for(var j=0;j<=sum;j++){
+            if(j===0){
+                subsum[a[i]][j]=1;
+            }else if(i===0){
+                subsum[a[i]][j]=(j===a[i])?1:0;
+            }else{
+                // put 1 if: 
+                // if previous row has 1 OR
+                // sum "j-a[i]" is possible in previous row
+                subsum[a[i]][j]= subsum[a[i-1]][j] || 
+                    (j-a[i]>=0 ? subsum[a[i-1]][j-a[i]] : 0);
+            }
+        }
+    }
+    console.log("Result: "+Boolean(subsum[a[i-1]][sum]));
+    console.log(JSON.stringify(subsum));
+    var subsets=[];
+    (function findsubsets(keyIndex, localSum, subsetIndex){
+        if(localSum==0){
+            return;
+        }
+        while(keyIndex>=0 && subsum[a[keyIndex]][localSum]==1){
+            // sum contendor only if number is less than the sum
+            if(a[keyIndex]<=localSum){
+                if(!subsets[subsetIndex]){
+                    subsets[subsetIndex]=[];
+                }
+                subsets[subsetIndex].push(a[keyIndex]);
+                findsubsets(keyIndex-1, localSum-a[keyIndex], subsetIndex);
+                subsetIndex++;
+            }
+            keyIndex--;
+        }
+    })(a.length-1, sum, 0)
+    console.log(JSON.stringify(subsets));
+}
+
+subsetSumDP([3,5,8,1,2,7],9);
